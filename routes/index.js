a1f8b8aea9f50ce02a484d890bc674888851af30
Dmitry <ds-8.5@bk.ru>
2023-08-19 12:59:21 +0300

    fix: исправил url ля авторизации пользователя

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,45 @@
 const router = require('express').Router();
+const {
+  celebrate,
+  Joi,
+} = require('celebrate');
 const usersRouter = require('./users');
 const cardsRouter = require('./cards');
 const auth = require('../middlewares/auth');
+const {
+  login,
+  createUser,
+} = require('../controllers/users');
 
 router.use('/users', usersRouter);
 router.use('/cards', auth, cardsRouter);
+usersRouter.post('/signin', celebrate({
+  body: Joi.object()
+    .keys({
+      email: Joi.string()
+        .required()
+        .email(),
+      password: Joi.string()
+        .required(),
+    }),
+}), login);
+usersRouter.post('/signup', celebrate({
+  body: Joi.object()
+    .keys({
+      email: Joi.string()
+        .required()
+        .email(),
+      password: Joi.string()
+        .required(),
+      name: Joi.string()
+        .min(2)
+        .max(30),
+      about: Joi.string()
+        .min(2)
+        .max(30),
+      avatar: Joi.string()
+        .pattern(/(http|https):\/\/(www.)?[-a-zA-Z0-9._~:/?#@!$&'()*,+;=]+#?/),
+    }),
+}), createUser);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,6 @@
 const {
   celebrate,
   Joi,
-  Segments,
 } = require('celebrate');
 const usersRouter = require('express')
   .Router();
@@ -10,46 +9,16 @@ const auth = require('../middlewares/auth');
 const {
   getUsers,
   getUserById,
-  createUser,
   updateProfileUser,
   updateAvatarUser,
-  login,
   getUserInfo,
 } = require('../controllers/users');
 
-usersRouter.post('/signin', celebrate({
-  [Segments.BODY]: Joi.object()
-    .keys({
-      email: Joi.string()
-        .required()
-        .email(),
-      password: Joi.string()
-        .required(),
-    }),
-}), login);
-usersRouter.post('/signup', celebrate({
-  [Segments.BODY]: Joi.object()
-    .keys({
-      email: Joi.string()
-        .required()
-        .email(),
-      password: Joi.string()
-        .required(),
-      name: Joi.string()
-        .min(2)
-        .max(30),
-      about: Joi.string()
-        .min(2)
-        .max(30),
-      avatar: Joi.string()
-        .pattern(/(http|https):\/\/(www.)?[-a-zA-Z0-9._~:/?#@!$&'()*,+;=]+#?/),
-    }),
-}), createUser);
 usersRouter.get('/me', auth, getUserInfo);
 usersRouter.get('/', auth, getUsers);
 usersRouter.get('/:userId', auth, getUserById);
 usersRouter.patch('/me', auth, celebrate({
-  [Segments.BODY]: Joi.object()
+  body: Joi.object()
     .keys({
       name: Joi.string()
         .required()
@@ -62,7 +31,7 @@ usersRouter.patch('/me', auth, celebrate({
     }),
 }), updateProfileUser);
 usersRouter.patch('/me/avatar', auth, celebrate({
-  [Segments.BODY]: Joi.object()
+  body: Joi.object()
     .keys({
       avatar: Joi.string()
         .required()
