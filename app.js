c7e34c6655fb7c5a2b71d62a51119c1c935ef175
Dmitry <ds-8.5@bk.ru>
2023-08-20 12:32:04 +0300

    fix: вынеc middleware централизованной обрботки ошибок в отдельную функцию

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const appRouter = require('./routes/index');
 const HTTP_ERRORS = require('./errors/errorCodes');
+const handlerError = require('./middlewares/handler-errors');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -16,21 +17,7 @@ app.use('/*', (req, res) => {
     .send({ message: 'Задан некорректный URL' });
 });
 app.use(errors());
-app.use((err, req, res, next) => {
-  const {
-    statusCode = 500,
-    message,
-  } = err;
-
-  res.status(statusCode)
-    .send({ message: statusCode === 500 ? 'На сервере произошла ошибка' : message });
-
-  next();
-});
-
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
-});
+app.use(handlerError);
 
 app.listen(PORT, () => {
 });
